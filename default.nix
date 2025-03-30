bab80bcd9641844842f66c90fca171b05526aaa6
Dane Rieber <danerieber@gmail.com>
2025-03-30 14:14:07 -0500

    update nix package

--- default.nix
+++ default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "hll-arty-tui";
-  version = "0.0.1";
+  version = "0.0.2";
 
   src = fetchFromGitHub {
     owner = "computerdane";
     repo = "hll-arty-tui";
     rev = "v${version}";
-    hash = "sha256-9bl+5Bk/xMSuf1iX5Vy3tZqCrATnpwIkMQeNERRJ+/U=";
+    hash = "sha256-019AzJtP45UIQBvOuPpd2FHfCDUxYPhcX+nauBFkC0c=";
   };
 
   vendorHash = "sha256-3NehgSR6y64/6VlvS7CcdAcekHTGXOHgWW0Zn3uX8hc=";
